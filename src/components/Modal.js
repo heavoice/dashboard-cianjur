d74da26dd4ee1d0169f3e4ec1944a2b45bb14827
wapiiqo <muhammadwafiq1313@gmail.com>
2025-06-10 13:34:40 +0700

    fix feedback

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,10 +2,12 @@ import { AiOutlineClose } from "react-icons/ai";
 import logo from "../assets/img/logo.png";
 import { useState } from "react";
 import { topik } from "../data/Topik";
+import { kepuasan } from "../data/Kepuasan";
 
 export const Modal = ({ onClose }) => {
   const API_URL = process.env.REACT_APP_API_URL;
   const [selectedTopics, setSelectedTopics] = useState([]);
+  const [selectedKepuasan, setSelectedKepuasan] = useState([]);
   const handleTopicClick = (id) => {
     setSelectedTopics(
       (prevSelected) =>
@@ -14,18 +16,29 @@ export const Modal = ({ onClose }) => {
           : [...prevSelected, id] // Tambahkan jika belum dipilih
     );
   };
+  const handleKepuasanClick = (id) => {
+    setSelectedKepuasan((prevSelected) =>
+      prevSelected.includes(id) ? [] : [id]
+    );
+  };
   const [nama, setNama] = useState("");
   const [pesan, setPesan] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     const selectedTopicTitles = topik
       .filter((item) => selectedTopics.includes(item.id))
       .map((item) => item.title);
+
+    const selectedKepuasanTitle =
+      kepuasan.find((item) => item.id === selectedKepuasan[0])?.title || "";
+
     const formData = new FormData();
     formData.append("nama", nama);
     formData.append("pesan", pesan);
     formData.append("topik", selectedTopicTitles.join(", "));
+    formData.append("kepuasan", selectedKepuasanTitle);
 
     const res = await fetch(`${API_URL}/umpan`, {
       method: "POST",
@@ -34,18 +47,20 @@ export const Modal = ({ onClose }) => {
 
     const result = await res.json();
     alert(result.message);
+
     setNama("");
     setPesan("");
     setSelectedTopics([]);
+    setSelectedKepuasan([]);
     onClose();
   };
   return (
     <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-md w-full md:max-w-xl xs:max-w-[25rem] max-w-[14rem] py-4 sm:py-12 relative">
+      <div className="bg-white rounded-lg shadow-md w-full md:max-w-xl xs:max-w-[25rem] max-w-[15rem] py-4 sm:py-12 relative h-fit max-h-[28rem] sm:max-h-[36rem] overflow-auto hide-scrollbar">
         {/* Header */}
         <div className="flex items-center justify-between border-b pb-3 sm:pb-6 mb-4 px-4 sm:px-12">
           <div className="font-black text-gray-900 flex items-center justify-center">
-            <img src={logo} className="w-7 h-7 sm:w-12 sm:h-12" />
+            <img src={logo} className="w-8 h-8 sm:w-12 sm:h-12" />
             <span className="ml-2 sm:ml-4 text-xs sm:text-base font-noto">
               Dashboard <br /> Cianjur
             </span>
@@ -88,12 +103,12 @@ export const Modal = ({ onClose }) => {
             <p className="block mb-2 text-sm font-semibold">
               Fitur apakah yang menurut anda sangat bermanfaat?
             </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 overflow-auto h-full max-h-[5rem] sm:max-h-full">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-2 overflow-auto h-full max-h-[5.6rem] md:max-h-full">
               {topik.map((item) => (
                 <button
                   key={item.id}
                   onClick={() => handleTopicClick(item.id)}
-                  className={`px-3 py-2 rounded-full text-sm transition border ${
+                  className={`px-3 py-2 rounded-full text-xxs sm:text-sm transition border ${
                     selectedTopics.includes(item.id)
                       ? "bg-[#22a9e1] text-white border-transparent"
                       : "border-gray-300 hover:bg-blue-100"
@@ -102,6 +117,39 @@ export const Modal = ({ onClose }) => {
                 >
                   {item.title}
                 </button>
+              ))}
+            </div>
+          </div>
+
+          <div className="px-4 sm:px-12">
+            <p className="block mb-2 text-sm font-semibold">
+              Sebarapa puaskah Anda dengan Dashboard Cianjur?
+            </p>
+            <div className="flex flex-row gap-2 items-center justify-center">
+              {kepuasan.map((item) => (
+                <div key={item.id} className="relative group">
+                  <button
+                    type="button"
+                    onClick={() => handleKepuasanClick(item.id)}
+                  >
+                    <img
+                      className="w-8 xs:w-14 md:w-20 lg:w-20 h-8 xs:h-14 md:h-20 lg:h-20"
+                      src={item.img}
+                    />
+                  </button>
+                  <div
+                    className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 
+                      bg-gray-800 text-white text-xs rounded py-1 px-2 
+                      ${
+                        selectedKepuasan.includes(item.id)
+                          ? "opacity-100"
+                          : "opacity-0 group-hover:opacity-100"
+                      }
+                      transition duration-300 pointer-events-none z-10 whitespace-nowrap`}
+                  >
+                    {item.title}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
---
+++ src/data/Kepuasan.js
@@ -0,0 +1,33 @@
+import SangatPuas from "../assets/img/sangatpuas.png";
+import Puas from "../assets/img/puas.png";
+import BiasaSaja from "../assets/img/biasa.png";
+import TidakPuas from "../assets/img/tidakpuas.png";
+import SangatTidakPuas from "../assets/img/sangattidakpuas.png";
+
+export const kepuasan = [
+  {
+    id: 1,
+    title: "Sangat Puas",
+    img: SangatPuas,
+  },
+  {
+    id: 2,
+    title: "Puas",
+    img: Puas,
+  },
+  {
+    id: 3,
+    title: "Biasa Saja",
+    img: BiasaSaja,
+  },
+  {
+    id: 4,
+    title: "Tidak Puas",
+    img: TidakPuas,
+  },
+  {
+    id: 5,
+    title: "Sangat Tidak Puas",
+    img: SangatTidakPuas,
+  },
+];
